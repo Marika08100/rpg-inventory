c3579c1c03b71233ca9b599dc7299556919ecee5
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-15 18:41:51 +0300

    is rdy

--- src/InventoryItem.java
+++ src/InventoryItem.java
@@ -1,14 +1,67 @@
+import java.util.Objects;
+
 public class InventoryItem {
     // Vegyél fel 3 fieldet: minden InventoryItem-nek legyen
-        // egy neve (name),
-        // egy mennyisége (quantity)
-        // és egy ára (price)
+    // egy neve (name),
+    // egy mennyisége (quantity)
+    // és egy ára (price)
+    private String name;
+    private int quantity;
+    private double price;
 
     // Írj egy konstruktort, ami beállítja az InventoryItem minden értékét!
+    InventoryItem(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+
+    }
 
     // Írj gettereket az összes fieldhez!
+    public String getName() {
+        return this.name;
+    }
 
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
     // Írj settert a mennyiséghez!
+    public void setMoreWithParameterQuantity(int earnQuantity){
+        this.quantity = earnQuantity;
+    }
+
+
+
 
     // Írj egy calculateValue() metódust, ami kiszámolja a termék össz. értékét (a mennyiség és egységár függvényében)
+    public double calculateValue() {
+        return this.price * this.quantity;
+    }
+    public boolean isEquals(InventoryItem other) {
+        return other.price == this.price && other.name.equals(this.name);
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryItem[" +
+                "name=" + name +
+                ", quantity=" + quantity +
+                ", price=" + price + "]";
+    }
 }
--- src/InventoryManager.java
+++ src/InventoryManager.java
@@ -10,32 +10,71 @@ public class InventoryManager {
     public void addItem(InventoryItem item) {
         // TODO
         // Add hozzá (az ebben az osztályban lévő) inventory változóhoz a paraméterként kapott itemet!
+        if (inventory.contains(item)) {
+            for (InventoryItem actual : inventory) {
+                if (actual.equals(item)) {
+                    actual.setMoreWithParameterQuantity(item.getQuantity());
+                    return;
+                }
+            }
+        }
+        inventory.add(item);
     }
 
     public void addItem(String name, int quantity, double price) {
         // TODO
         // Hívd meg az InventoryItem kosntruktorát a három paraméterrel és mentsd el egy item változóba!
-        // Add hozzá az inventoryhoz ezt az item-et!
+       // Add hozzá az inventoryhoz ezt az item-et!
+      InventoryItem item = new InventoryItem(name, quantity, price);
+       inventory.add(item);
+        //addItem(name, quantity, price);
     }
 
     public void printItems() {
         // TODO
+        for (InventoryItem item : inventory) {
+            System.out.println("Name : " + item.getName());
+            System.out.println("Quantity" + item.getQuantity());
+            System.out.println("Price " + item.getPrice());
+
+            System.out.println(inventory.toString());
+        }
     }
 
     public void removeItem(String name) {
         // TODO
+        for (InventoryItem actual : inventory) {
+            if (actual.getName().equals(name)) {
+                inventory.remove(actual);
+                return;
+            }
+
+
+        }
     }
 
     public void updateQuantity(String name, int newQuantity) {
         // TODO
+        for (InventoryItem actual : inventory) {
+            if (actual.getName().equals(name)) {
+                actual.setQuantity(newQuantity);
+                return;
+            }
+
+
+        }
+
+
     }
 
     public double calculateTotalValue() {
         // TODO
-        return 0;
+        int sum = 0;
+        for (InventoryItem actual : inventory) {
+            sum += actual.calculateValue();
+        }
+        return sum;
     }
-
-
 
 
 }
